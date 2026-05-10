d866c37881d8f0fc15857a1ae28741c18530cb55
Hemanth Krishna <dandamudihk@gmail.com>
2026-05-10 11:16:21 +0530

    Fixed CORS for Render deployment

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://YOUR_FRONTEND_DOMAIN.vercel.app',
-    ],
+    origin: '*',
     credentials: true,
   })
 );
